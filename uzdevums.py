ac2fc9520bd67a8eac5cd74b21be59f84bae690c
deniizeskujaa <denize@hotmail.lv>
2024-10-30 15:16:37 +0200

    Update uzdevums.py

--- uzdevums.py
+++ uzdevums.py
@@ -1,1 +1,12 @@
-print("Hello")
+"""
+Izveidot programmu, kur jāatbild uz 4 jautājumiem.
+Pēc atbilžu ievadīšanas tiek izvadīti iegūtie rezultāti.
+"""
+
+def uzdevums1(a,b,c,d):
+    print("Tev patīk",a)
+    print("Tev nepatīk",b)
+    print("Tev garšo",c)
+    print("Tev negaršo",d)
+    
+uzdevums1("suņi","kaķi","saldējums","griķi")
